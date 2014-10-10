6c7ae9f450f86c5fa5bff9944ce74c7510f13311
Brady Watkinson <dabradybunch1@hotmail.com>
2014-10-10 21:09:20 +1100

    pretty much finished this script. All tables complete and all parsing is easy to adjust if I want to change the format of any column

--- database/extract_data_student.pl
+++ database/extract_data_student.pl
@@ -2,32 +2,105 @@
 
 use DBI;
 
+my $id = 100000;
+my @student_attributes = ('name','birthdate','degree','email','gender','hair_colour',
+			'height','password','username','weight');
+my @favourite_categories = ('favourite_TV_shows','favourite_bands','favourite_books','favourite_hobbies','favourite_movies');
+#=========Creates SQL tables=========
+
 #---open connection to db---
 $dbh = DBI->connect('dbi:SQLite:student_DB', 'root', 'password') or die;
 
-my $id = 100000;
-
-my @student_attributes = ('name','birthdate','degree','email','gender','hair_colour',
-			'height','password','username','weight');
-
-
-#=========Creates SQL tables=========
 #.........Student Table..............
-my $sql = "DROP TABLE IF EXISTS students";
-	$sth = $dbh->prepare($sql);
+my $sql_drop_users = "DROP TABLE IF EXISTS users";
+	$sth = $dbh->prepare($sql_drop_users);
 	$sth->execute or die "SQL Error: $DBI::errstr\n";
 	while (@row = $sth->fetchrow_array) {
 		print "@row\n";
 	}
 
-my $sql = "CREATE TABLE IF NOT EXISTS students(user_id char(6), first_name varchar, last_name varchar, gender char(1), dob varchar, email varchar, hair_colour varchar, degree varchar, height varchar, weight varchar, username varchar, password varchar)";
-	$sth = $dbh->prepare($sql);
-	$sth->execute or die "SQL Error: $DBI::errstr\n";
+my $sql_make_users = "
+	CREATE TABLE IF NOT EXISTS users(
+	user_id char(6) PRIMARY KEY, 
+	first_name 		VARCHAR, 
+	last_name 		VARCHAR, 
+	gender 			CHAR(1), 
+	dob 				VARCHAR, 
+	email 			VARCHAR, 
+	hair_colour 	VARCHAR, 
+	degree 			VARCHAR, 
+	height 			VARCHAR, 
+	weight 			VARCHAR, 
+	username 		VARCHAR, 
+	password 		VARCHAR)";
+$sth = $dbh->prepare($sql_make_users);
+$sth->execute or die "SQL Error: $DBI::errstr\n";
+while (@row = $sth->fetchrow_array) {
+	print "@row\n";
+}	
+
+#.........Courses Table..............
+my $sql_drop_user_courses = "DROP TABLE IF EXISTS user_courses";
+$sth = $dbh->prepare($sql_drop_user_courses);
+$sth->execute or die "SQL Error: $DBI::errstr\n";
+while (@row = $sth->fetchrow_array) {
+	print "@row\n";
+}
+
+my $sql_make_user_courses = "
+	CREATE TABLE IF NOT EXISTS user_courses(
+	course_code 		CHAR(8), 
+	year 					CHAR(4),
+	semester 			CHAR(2),
+	user_id 				CHAR(6),
+	FOREIGN KEY(user_id)
+		REFERENCES users(user_id)
+		ON DELETE CASCADE
+	PRIMARY KEY(user_id, course_code, semester, year))";
+$sth = $dbh->prepare($sql_make_user_courses);
+$sth->execute or die "SQL Error: $DBI::errstr\n";
+while (@row = $sth->fetchrow_array) {
+	print "@row\n";
+}
+
+#.........Favourites Table..............
+my $sql_drop_user_favourites = "DROP TABLE IF EXISTS user_favourites";
+$sth = $dbh->prepare($sql_drop_user_favourites);
+$sth->execute or die "SQL Error: $DBI::errstr\n";
+while (@row = $sth->fetchrow_array) {
+	print "@row\n";
+}
+
+my $sql_make_user_favourites = "
+	CREATE TABLE user_favourites(
+	user_id CHAR(6), 
+	type_id VARCHAR,
+	name VARCHAR,
+	FOREIGN KEY(user_id)
+		REFERENCES users(user_id)
+		ON DELETE CASCADE
+	PRIMARY KEY(user_id, name))";
+$sth = $dbh->prepare($sql_make_user_favourites);
+$sth->execute or die "SQL Error: $DBI::errstr\n";
+while (@row = $sth->fetchrow_array) {
+	print "@row\n";
+}
+
+#=========End Create Tables===========
+
+#=========sql functions===========
+
+sub queryTable
+{
+	my ($query_table_name) = (@_); 
+	my $sql_query = "select * from $query_table_name";
+	my $sth_query = $dbh->prepare($sql_query);
+	$sth_query->execute or die "SQL Error: $DBI::errstr\n";
 	while (@row = $sth->fetchrow_array) {
 		print "@row\n";
 	}	
-
-#=========End Create Table===========
+}
+#=========end sql functions========
 
 foreach $dir (@ARGV) {
 	open F, "<$dir/profile.txt" or die;
@@ -35,13 +108,24 @@ foreach $dir (@ARGV) {
 	$file = join '', @file;
 	@chunk = split '\n', $file;
 	
-	my %student;
+	my %users;
+	my @user_courses_course_code;
+	my @user_courses_year;
+	my @user_courses_semester;
+	my %favourites;
 	foreach $line (@chunk) {
+		$line =~ s/\t//;
 		if ($line =~ s/^(\w+):$/$1/) {
 			$category = $1;
-		} elsif ($category ~~ @student_attributes) {
-			$line =~ s/\t//;
-			$student{$category} = $line;
+		} elsif ($category ~~ @student_attributes) {		
+			$users{$category} = $line;
+		} elsif ($category eq "courses") {
+			my @course_items = split ' ', $line;
+			push @user_courses_year, 			$course_items[0];
+			push @user_courses_semester, 		$course_items[1];
+			push @user_courses_course_code, 	$course_items[2];
+		} elsif ($category ~~ @favourite_categories) {
+			$favourites{$line} = $category;
 		}
 	}
 	
@@ -50,32 +134,71 @@ foreach $dir (@ARGV) {
 		#printf "%12s: $student{$key}\n", $key;
 	#}
 	
-	$student{"name"} =~ /(\w+) (\w*)/;
-	$f_name 	= $1 || "";
-	$l_name 	= $2 || "";
-	$gender 	= $student{"gender"} || "";
-	$dob 		= $student{"birthdate"} || "";
-	$email	= $student{"email"} || "";
-	$hair		= $student{"hair_colour"} || "";
-	$degree	= $student{"degree"} || "";
-	$height	= $student{"height"} || "";
-	$weight	= $student{"weight"} || "";
-	$u_name	= $student{"username"} || "";
-	$pass		= $student{"password"} || "";
+	#=====insert into user table=====
+	$users{"name"} =~ /(\w+) ?(\w*)/;
+	my $f_name 	= $1 || "";
+	my $l_name 	= $2 || "";
+	my $gender 	= $users{"gender"} || "";
+	my $dob 		= $users{"birthdate"} || "";
+	my $email	= $users{"email"} || "";
+	my $hair		= $users{"hair_colour"} || "";
+	my $degree	= $users{"degree"} || "";
+	my $height	= $users{"height"} || "";
+	my $weight	= $users{"weight"} || "";
+	my $u_name	= $users{"username"} || "";
+	my $pass		= $users{"password"} || "";
 	 
-	my $sql = "insert into students values ('$id','$f_name','$l_name','$gender','$dob','$email','$hair','$degree','$height','$weight','$u_name','$pass')";
-	print "$sql\n";
-	$sth = $dbh->prepare($sql);
+	my $sql_insert_users = "insert into users values ('$id','$f_name','$l_name','$gender','$dob','$email','$hair','$degree','$height','$weight','$u_name','$pass')";
+	print "$sql_insert_users\n";
+	$sth = $dbh->prepare($sql_insert_users);
 	$sth->execute or die "SQL Error: $DBI::errstr\n";	
-	#---open connection to db---
-	$sql = "select * from students";
-	$sth = $dbh->prepare($sql);
-	$sth->execute or die "SQL Error: $DBI::errstr\n";
-	while (@row = $sth->fetchrow_array) {
-		print "@row\n";
+	
+	#$sql = "select * from users";
+	#$sth = $dbh->prepare($sql);
+	#$sth->execute or die "SQL Error: $DBI::errstr\n";
+	#while (@row = $sth->fetchrow_array) {
+	#	print "@row\n";
+	#}
+	
+	#=====insert into user_courses table=====
+	foreach $i (0..$#user_courses_course_code) {
+		my $course_code	= $user_courses_course_code[$i] || "";
+		my $year				= $user_courses_year[$i] || "";
+		my $semester		= $user_courses_semester[$i] || "";
+		my $sql_insert_users_courses = "insert into user_courses values ('$course_code','$year','$semester','$id')";
+		#print "$sql_insert_users_courses\n";
+		$sth = $dbh->prepare($sql_insert_users_courses);
+		$sth->execute or die "SQL Error: $DBI::errstr\n";	
 	}
 	
+	#$sql = "select * from user_courses";
+	#$sth = $dbh->prepare($sql);
+	#$sth->execute or die "SQL Error: $DBI::errstr\n";
+	#while (@row = $sth->fetchrow_array) {
+	#	print "@row\n";
+	#}	
+	
+	#=====insert into user_courses table=====
+	foreach $key (keys %favourites) {
+		my $type		= $favourites{$key} || "";
+	  (my $name		= $key) =~ s/'/''/g;
+		my $sql_insert_users_favourites = "insert into user_favourites values ('$id','$type','$name')";
+		#print "$sql_insert_users_favourites\n";
+		$sth = $dbh->prepare($sql_insert_users_favourites);
+		$sth->execute or die "SQL Error: $DBI::errstr\n";	
+	}
+	
+	#$sql = "select * from user_favourites";
+	#$sth = $dbh->prepare($sql);
+	#$sth->execute or die "SQL Error: $DBI::errstr\n";
+	#while (@row = $sth->fetchrow_array) {
+	#	print "@row\n";
+	#}
+	#
 	$id += 10;
+	
+	
+	
 }
 
 #---close connection to db---
